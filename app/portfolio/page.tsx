e597fb21c93622baca5c18fa306d3a17e8bb674b
Mario333333 <mariocgc@icloud.com>
2024-07-10 13:00:28 -0600

    test para compilacion

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default async function CV() {
   return (
     <div className={styles.main}>
-      <h1>Building ...</h1>
+      <h1>Building...</h1>
     </div>
   );
 }
