291611ccfd3a73f59e3f81cb145a8472c801b6a0
Mario333333 <mariocgc@icloud.com>
2024-07-11 17:24:27 -0600

    actualizando empleos

--- app/utils/JobCardConstants.ts
+++ app/utils/JobCardConstants.ts
@@ -1,29 +1,30 @@
 export const jobTitle = "Professional Experience";
 
 export const jobs = [
-    {
-      company: "Fonyou",
-      position: "Front-End Engineer",
-      activities: "Maintenance of sites and create new features and proyects.",
-      duration: "From 18/10/2021 to the present day",
-    },
-    {
-      company: "Pleed Tech",
-      position: "Front-End Engineer",
-      activities: "Maintenance of sites and create new features and proyects.",
-      duration: "From 16/10/2019 to to 15/10/2021",
-    },
-    {
-      company: "Alucina Studio",
-      position: "Front-End Engineer",
-      activities: "Maintenance of sites and create new features and proyects.",
-      duration: "From 6/08/2018 to 15/10/2019",
-    },
-    {
-      company: "Redes y soporte en microcomputación",
-      position: "Front-End Engineer",
-      activities: "Maintenance of sites and create new features and proyects.",
-      duration: "From 4/09/2012 to 21/01/2014",
-    },
-  ];
-  
+  {
+    company: "Fonyou",
+    position: "Front-End Engineer",
+    activities:
+      "Activities: maintenance of sites; redesign of the Channel Web Telcel Site. Monitoring of pods and setting CI and CD with Jenkins, GitLab, and GCP for the projects. We used agile methodologies like scrum. We used git for version control and Jira for task management.",
+    duration: "From 18/10/2021 to 06/07/2024",
+  },
+  {
+    company: "Pleed Tech",
+    position: "Front-End Engineer",
+    activities: "Activities: I maintain and create new features for the CRM system.",
+    duration: "From 16/10/2019 to to 15/10/2021",
+  },
+  {
+    company: "Alucina Studio",
+    position: "Full Stack Developer",
+    activities:
+      "Activities: We created sites and mobile applications for iOS and Android platforms. We used technologies like React, React Native, Node.js, and MongoDB.",
+    duration: "From 6/08/2018 to 15/10/2019",
+  },
+  {
+    company: "Redes y soporte en microcomputación",
+    position: "Technical Support",
+    activities: "Activities: Technical support for Dell computers",
+    duration: "From 4/09/2012 to 21/01/2014",
+  },
+];
